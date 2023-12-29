b38630a8ea63f55087cc1558e8710da572431c90
nirzaf <mfmfazrin1986@gmail.com>
2023-12-29 12:21:12 +0300

    Refactor WeatherCommands to use constructor injection
    This revision simplifies WeatherCommands by replacing a private field with a constructor parameter. The constructor's previous functionality (assignment of `weatherService` to a private field) is now achieved implicitly. The Assembly Informational Version Attribute has also been updated.

--- CreatingCliTools/WeatherCommands.cs
+++ CreatingCliTools/WeatherCommands.cs
@@ -4,26 +4,19 @@ using CreatingCliTools.Weather;
 
 namespace CreatingCliTools;
 
-public class WeatherCommands
+public class WeatherCommands(IWeatherService weatherService)
 {
-    private readonly IWeatherService _weatherService;
-
-    public WeatherCommands(IWeatherService weatherService)
-    {
-        _weatherService = weatherService;
-    }
-
     [Command("weather")]
     public async Task Weather1(string city)
     {
-        var weather = await _weatherService.GetWeatherForCityAsync(city);
+        var weather = await weatherService.GetWeatherForCityAsync(city);
         Console.WriteLine(JsonSerializer.Serialize(weather, new JsonSerializerOptions{WriteIndented = true}));
     }
     
     [Ignore]
     public async Task Weather2(string city)
     {
-        var weather = await _weatherService.GetWeatherForCityAsync(city);
+        var weather = await weatherService.GetWeatherForCityAsync(city);
         Console.WriteLine(JsonSerializer.Serialize(weather, new JsonSerializerOptions{WriteIndented = true}));
     }
 }
--- CreatingCliTools/obj/Debug/net8.0/CreatingCliTools.AssemblyInfo.cs
+++ CreatingCliTools/obj/Debug/net8.0/CreatingCliTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreatingCliTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef7d7f8191e67bac104ae86c4123bcf0d938be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreatingCliTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreatingCliTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
